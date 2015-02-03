0f17dc8ae841561e42aba226057b325247cd1184
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2015-02-03 22:59:44 +0100

    demo looks OK

--- demo.py
+++ demo.py
@@ -3,12 +3,12 @@ import ads
 
 lk="demo.link"
 
-#marche pas
-#ads.makelink(n="Feynman",fn="Richard",ini="R.",name=lk)
+ads.makelink(n="Feynman",fn="Richard",ini="R.P.",name=lk)
 
 ads.makepage('feynman',\
              retrieve = True,\
-             listyear = range(1990,1938,-1),\
              linkads = lk,\
+             listyear = range(2015,1900,-1),\
              title = "<CENTER><H2><EM><font color='#B8860B;'>Richard Feynman's publications</font></EM></H2></CENTER>",
              target = 'demo')
+
